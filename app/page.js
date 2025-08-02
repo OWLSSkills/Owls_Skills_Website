6d059127b74a25a83521a1c4ff627eaabede67b2
Jacob Diehl <jakediehl17@gmail.com>
2025-08-02 07:49:03 -0600

    add owls banner and positioned

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-
+import HeroLanding from "@/components/Hero_Landing/Hero_Landing";
 export default function Home() {
   return (
     <div className={styles.page}>
-      
+      <HeroLanding/>
     </div>
   );
 }
--- components/Hero_Landing/Hero_Landing.jsx
+++ components/Hero_Landing/Hero_Landing.jsx
@@ -1,10 +1,19 @@
-
+import styles from './Hero_landing.module.css';
 
 export default function HeroLanding() {
 
     return (
-        <div className="hero_landing">
-
+        <div className={styles.hero_landing}>
+            <div className={styles.hero_landing_topagraphic_background}>
+                <div className={styles.hero_landing_images_container}> 
+                    <img src='/O.W.L.S_banner.png' alt='Topographic Background' className={styles.OWLS_banner}/>
+                    <div></div>
+                </div>
+                <div className={styles.hero_landing_buttons_container}>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
         </div>
     )
 }
