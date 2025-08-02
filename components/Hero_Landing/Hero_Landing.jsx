ac9e3d8d8b5961d2b85bc8a9700191365f7b2e49
Jacob Diehl <jakediehl17@gmail.com>
2025-08-02 08:11:30 -0600

    The images are in. However, when adjusting screen size they begin miss aligning. need to adjust

--- components/Hero_Landing/Hero_Landing.jsx
+++ components/Hero_Landing/Hero_Landing.jsx
@@ -5,9 +5,17 @@ export default function HeroLanding() {
     return (
         <div className={styles.hero_landing}>
             <div className={styles.hero_landing_topagraphic_background}>
-                <div className={styles.hero_landing_images_container}> 
-                    <img src='/O.W.L.S_banner.png' alt='Topographic Background' className={styles.OWLS_banner}/>
-                    <div></div>
+                <div className={styles.hero_landing_images_container}>
+                <img
+                    src="/O.W.L.S_banner.png"
+                    alt="OWLS Banner"
+                    className={styles.OWLS_banner}
+                />
+                <img
+                    src="/pictures_first_banner.png"
+                    alt="People having fun"
+                    className={styles.pictures_first_banner}
+                />
                 </div>
                 <div className={styles.hero_landing_buttons_container}>
                     <div></div>
