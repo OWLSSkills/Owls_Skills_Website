3b7299ee5a3dc0554fbfe72b7b4533fe1770e28b
Jacob Diehl <jakediehl17@gmail.com>
2025-08-30 13:05:22 -0600

    Adjust socials placement

--- components/LayoutComponents/Footer/Footer.jsx
+++ components/LayoutComponents/Footer/Footer.jsx
@@ -1,14 +1,24 @@
 import styles from './Footer.module.css';
+import SocialIcons from '@/components/Home/Socials/Socials';
 
 export default function Footer() {
-    const currentYear = new Date().getFullYear();
-    return (
-        <footer className={styles.footer}>
-            <div className={`${styles.footerStack}`}>
-                <img src="/images/Jessie_Krebs_Color.png" alt="Jessie Krebs Logo" className={styles.smallLogo} />
-                <p>© {currentYear} Jessie Krebs.</p>
-                <p>All rights reserved.</p>
-            </div>
-        </footer>
-    )
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footerStack}>
+        <img
+          src="/images/Jessie_Krebs_Color.png"
+          alt="Jessie Krebs Logo"
+          className={styles.smallLogo}
+        />
+        <p>© {currentYear} Jessie Krebs.</p>
+        <p>All rights reserved.</p>
+      </div>
+
+      {/* docked on the right + scaled */}
+      <div className={`${styles.socialsDock} ${styles.shrink_socials}`}>
+        <SocialIcons />
+      </div>
+    </footer>
+  );
 }
